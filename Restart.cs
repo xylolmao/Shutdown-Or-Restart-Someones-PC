5788c5a785c075217f6e5f8ef628ff2501d4a92a
xylo <104737367+xylolmao@users.noreply.github.com>
2022-05-01 23:23:59 +0100

    Add files via upload

---
+++ Restart.cs
@@ -0,0 +1,16 @@
+﻿// Made By xylo#6666
+
+//Imports:
+using System.Diagnostics;
+
+class Restart
+{
+    static void Main(string[] args)
+    {
+        string filename = "shutdown.exe";
+        string arg = "-r";
+        var start = new ProcessStartInfo(filename, arg);
+        Process.Start(start);
+    }
+}
+
---
+++ Shutdown.cs
@@ -0,0 +1,15 @@
+﻿// Made By xylo#6666
+
+// Imports:
+using System.Diagnostics;
+
+class Shutdown
+{
+    static void Main(string[] args)
+    {
+        string filename = "shutdown.exe";
+        string arg = "-s";
+        var start = new ProcessStartInfo(filename, arg);
+        Process.Start(start);
+    }
+}
